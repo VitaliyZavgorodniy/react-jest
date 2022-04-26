398def3b5dae6af6e028785b47470df7e7df1d9a
Vitaliy Zavgorodniy <49337282+VitaliyZavgorodniy@users.noreply.github.com>
2022-04-26 22:07:24 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import App from 'App';
+import store from 'store';
+
+import 'normalize.css';
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
